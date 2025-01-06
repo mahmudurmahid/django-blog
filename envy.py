de8f7339e8879da645978070ef8029517661234c
Mahmudur Mahid <m.mahid@icloud.com>
2025-01-06 13:06:07 +0000

    Update envy.py

--- envy.py
+++ envy.py
@@ -1,2 +1,5 @@
 import os
 os.environ.setdefault('DEBUG', '1')
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:JV5buU8xzekv@ep-blue-pine-a275k8ys.eu-central-1.aws.neon.tech/staff_guru_salt_893997")
