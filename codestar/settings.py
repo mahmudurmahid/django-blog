36e6878bab182b778d2ae6ef50abe72caab25636
Mahmudur Mahid <m.mahid@icloud.com>
2025-01-06 11:20:08 +0000

    Update settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
-from pathlib import Path
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
+from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
