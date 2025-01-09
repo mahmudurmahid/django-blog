8f36a155890ba64a9f6e6f85d12007c7accac3d9
Mahmudur Mahid <m.mahid@icloud.com>
2025-01-09 11:52:30 +0000

    added excerpt and updated post model

--- blog/models.py
+++ blog/models.py
@@ -14,4 +14,4 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
-    updated_on = model.DateTimeField(auto_now=True)
+    updated_on = models.DateTimeField(auto_now=True)
