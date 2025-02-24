e071c676e3af0e36fc3637c92d5caba6b41ffea9
mahmudurmahid <m.mahid@icloud.com>
2025-02-24 16:39:17 +0000

    heroku stuff wink

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1ec#b-k1pupf!aj@ri+tqxrxr33hukb$*4@vb+j4b&__p=cmiz'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEBUG' in os.environ
