d1526e5f63697563107296c0539f5d6178e90ff9
mahmudurmahid <m.mahid@icloud.com>
2025-02-25 16:18:38 +0000

    add meta class to Post model

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,12 @@ class Post(models.Model):
     exerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['created_on', 'author']
+    
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='commenter')
